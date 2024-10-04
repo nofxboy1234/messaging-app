3e44d97c911c3999446a6a0c1a91da4b993934af
Dylan Palmboom <nofxboy1234@gmail.com>
2024-10-04 10:07:43 +0200

    Install and configure vitest and react testing library

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import RubyPlugin from 'vite-plugin-ruby'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import RubyPlugin from 'vite-plugin-ruby';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    RubyPlugin(),
-  ],
-})
+  plugins: [react(), RubyPlugin()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests/setup.js',
+  },
+});
