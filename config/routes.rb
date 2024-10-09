91084bb96ed3942cbf0910d600d95aa0da5e3018
Dylan Palmboom <nofxboy1234@gmail.com>
2024-10-09 14:50:22 +0200

    Generate devise controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  # devise_for :users, controllers: { sessions: "users/sessions" }
   root "inertia_example#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
