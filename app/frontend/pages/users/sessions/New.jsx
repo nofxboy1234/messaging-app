5b16059f1f3fe0b51d600db1de57ae809cb5bb73
Dylan Palmboom <nofxboy1234@gmail.com>
2024-10-09 21:05:13 +0200

    Implement users/sessions#new react component

--- app/frontend/pages/users/sessions/New.jsx
+++ app/frontend/pages/users/sessions/New.jsx
@@ -1,5 +1,60 @@
+import { router } from '@inertiajs/react';
+import { useState } from 'react';
+
 function New() {
-  return <div>New</div>;
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+    remember_me: false,
+  });
+
+  function handleChange(e) {
+    const key = e.target.id;
+    const value = e.target.value;
+    setValues((values) => ({
+      ...values,
+      [key]: value,
+    }));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const userData = {
+      user: {
+        email: values.email,
+        password: values.password,
+        remember_me: values.remember_me,
+      },
+    };
+    router.post('/users/sign_in', userData);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="email">Email:</label>
+      <input
+        type="email"
+        id="email"
+        value={values.email}
+        onChange={handleChange}
+      />
+      <label htmlFor="password">Password:</label>
+      <input
+        type="password"
+        id="password"
+        value={values.password}
+        onChange={handleChange}
+      />
+      <input
+        type="checkbox"
+        id="remember_me"
+        value={values.remember_me}
+        onChange={handleChange}
+      />
+
+      <button type="submit">Log in</button>
+    </form>
+  );
 }
 
 export default New;
