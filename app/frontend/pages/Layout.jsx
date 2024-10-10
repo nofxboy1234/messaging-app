4dec558041f63f08d950d020fd6c38eef1d6082c
Dylan Palmboom <nofxboy1234@gmail.com>
2024-10-10 13:49:13 +0200

    Add Layout and New

---
+++ app/frontend/pages/Layout.jsx
@@ -0,0 +1,29 @@
+import { Link, usePage } from '@inertiajs/react';
+
+export default function Layout({ children }) {
+  const url = usePage().url;
+  const { shared } = usePage().props;
+
+  let deviseLink = null;
+
+  if (url != '/users/sign_in') {
+    deviseLink = shared.current_user ? (
+      <Link href="/users/sign_out" method="delete" as="button">
+        Sign out
+      </Link>
+    ) : (
+      <Link href="/users/sign_in">Sign in</Link>
+    );
+  }
+
+  return (
+    <main>
+      <header>
+        {shared.flash.alert && <div>{shared.flash.alert}</div>}
+        {shared.flash.notice && <div>{shared.flash.notice}</div>}
+        {deviseLink}
+      </header>
+      <article>{children}</article>
+    </main>
+  );
+}
--- app/frontend/pages/sessions/New.jsx
+++ app/frontend/pages/sessions/New.jsx
@@ -1,5 +1,73 @@
+import { router } from '@inertiajs/react';
+import { useState } from 'react';
+import Layout from '../../Layout';
+
 function New() {
-  return <div>New Session</div>;
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+    remember_me: false,
+  });
+
+  function handleChange(e) {
+    const key = e.target.id;
+    const value = e.target.value;
+    setValues((values) => ({
+      ...values,
+      [key]: value,
+    }));
+  }
+
+  function handleCheckboxChange(e) {
+    const key = e.target.id;
+    const value = values.remember_me;
+    setValues((values) => ({
+      ...values,
+      [key]: !value,
+    }));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const userData = {
+      user: {
+        email: values.email,
+        password: values.password,
+        remember_me: values.remember_me,
+      },
+    };
+    router.post('/users/sign_in', userData);
+  }
+
+  return (
+    <Layout>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={values.email}
+          onChange={handleChange}
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={values.password}
+          onChange={handleChange}
+        />
+        <label htmlFor="remember_me">Remember me:</label>
+        <input
+          type="checkbox"
+          id="remember_me"
+          value={values.remember_me}
+          onChange={handleCheckboxChange}
+        />
+
+        <button type="submit">Log in</button>
+      </form>
+    </Layout>
+  );
 }
 
 export default New;
