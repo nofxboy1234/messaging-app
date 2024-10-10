6cb4b9e5aa5949b7471b00c2672ad674a9b4c712
Dylan Palmboom <nofxboy1234@gmail.com>
2024-10-10 09:12:58 +0200

    Conditionally render sign in and sign out buttons

--- app/frontend/pages/Layout.jsx
+++ app/frontend/pages/Layout.jsx
@@ -8,10 +8,14 @@ export default function Layout({ children }) {
       <header>
         {shared.flash.alert && <div>{shared.flash.alert}</div>}
         {shared.flash.notice && <div>{shared.flash.notice}</div>}
-        <Link href="/users/sign_out" method="delete" as="button">
-          Sign out
-        </Link>
-        <Link href="/users/sign_in">Sign in</Link>
+
+        {shared.current_user ? (
+          <Link href="/users/sign_out" method="delete" as="button">
+            Sign out
+          </Link>
+        ) : (
+          <Link href="/users/sign_in">Sign in</Link>
+        )}
       </header>
       <article>{children}</article>
     </main>
