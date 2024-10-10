3d37b10d0a7f001a3e6acada7c1db901e0e3ecf2
Dylan Palmboom <nofxboy1234@gmail.com>
2024-10-10 16:43:25 +0200

    Delete migration that drops :users table

--- db/migrate/20241010100259_drop_users_table.rb
+++
@@ -1,9 +0,0 @@
-class DropUsersTable < ActiveRecord::Migration[7.2]
-  def up
-    drop_table :users
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration, "This migration cannot be reverted because it destroys data."
-  end
-end
