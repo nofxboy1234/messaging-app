db906255edb449756400a7b3c52c1b595cc2506b
Dylan Palmboom <nofxboy1234@gmail.com>
2024-10-11 14:51:52 +0200

    Create subscription on consumer inside Home useEffect

--- app/frontend/channels/message_channel.js
+++ app/frontend/channels/message_channel.js
@@ -2,18 +2,18 @@ console.log('*** message_channel.js');
 
 import consumer from './consumer';
 
-const messageChannel = consumer.subscriptions.create('MessageChannel', {
-  connected() {
-    console.log('*** frontend message channel connected');
-  },
+// const messageChannel = consumer.subscriptions.create('MessageChannel', {
+//   connected() {
+//     console.log('*** frontend message channel connected');
+//   },
 
-  disconnected() {
-    console.log('*** frontend message channel disconnected');
-  },
+//   disconnected() {
+//     console.log('*** frontend message channel disconnected');
+//   },
 
-  received(data) {
-    console.log('*** frontend message channel received');
-  },
-});
+//   received(data) {
+//     console.log('*** frontend message channel received');
+//   },
+// });
 
-export { consumer, messageChannel };
+export { consumer };
--- app/frontend/pages/home/Index.jsx
+++ app/frontend/pages/home/Index.jsx
@@ -1,11 +1,25 @@
 import { Link } from '@inertiajs/react';
 import Layout from '../Layout';
 import { useEffect } from 'react';
-import { consumer, messageChannel } from '../../channels/message_channel';
+import { consumer } from '../../channels/message_channel';
 
 function Home({ session }) {
   useEffect(() => {
     console.log('*** Home useEffect');
+
+    const messageChannel = consumer.subscriptions.create('MessageChannel', {
+      connected() {
+        console.log('*** frontend message channel connected');
+      },
+
+      disconnected() {
+        console.log('*** frontend message channel disconnected');
+      },
+
+      received(data) {
+        console.log('*** frontend message channel received');
+      },
+    });
   }, []);
 
   console.log('*** Home rendering');
