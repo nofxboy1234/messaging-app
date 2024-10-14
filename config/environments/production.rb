bc40440c3f22b5edb01cbfc87dfed9c1e0df2087
Dylan Palmboom <nofxboy1234@gmail.com>
2024-10-14 14:18:00 +0200

    Test that development action cable is sending messages to 2 users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,9 +34,10 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = "wss://example.com/cable"
-  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
+  config.action_cable.mount_path = "/cable"
+  config.action_cable.url = "wss://glacial-lowlands-89807-117906cec308.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = [ "https://glacial-lowlands-89807-117906cec308.herokuapp.com",
+                                                  /http:\/\/glacial-lowlands-89807-117906cec308.herokuapp.com/ ]
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
--- config/routes.rb
+++ config/routes.rb
@@ -41,4 +41,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  mount ActionCable.server => "/cable"
 end
