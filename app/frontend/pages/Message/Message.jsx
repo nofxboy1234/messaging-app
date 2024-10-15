91c45ea37d7144f15c5f461405174ce729937abb
Dylan Palmboom <nofxboy1234@gmail.com>
2024-10-15 23:05:58 +0200

    Style Message

--- app/frontend/pages/Message/Message.jsx
+++ app/frontend/pages/Message/Message.jsx
@@ -1,6 +1,8 @@
+import styles from './Message.module.css';
+
 export default function Message({ message }) {
   return (
-    <div>
+    <div className={styles.message}>
       <div>{message.username.toString()}</div>
       <div>{message.body.toString()}</div>
     </div>
