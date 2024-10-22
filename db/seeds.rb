fd4496302134a6ae9d80cd322972056ba54dd136
Dylan Palmboom <nofxboy1234@gmail.com>
2024-10-22 12:25:13 +0200

    Add db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,24 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+user1 = User.create!(email: 'user1@example.com', password: 'user1!@#$%^&')
+user2 = User.create!(email: 'user2@example.com', password: 'user2!@#$%^&')
+Profile.create!(
+                username: user1.email.split('@').first,
+                picture: 'https://example.com/pictures/user1',
+                about: "Hello, I am #{user1.email.split('@').first}!",
+                user_id: user1.id
+                )
+Profile.create!(
+  username: user2.email.split('@').first,
+  picture: 'https://example.com/pictures/user2',
+  about: "Hello, I am #{user2.email.split('@').first}!",
+  user_id: user2.id
+  )
+
+
+chat1 = Chat.create!(name: 'Chat1')
+
+message1 = Message.create!(body: 'Hello user2!', user_id: user1.id, chat_id: chat1.id)
+message2 = Message.create!(body: 'Hello user1!', user_id: user2.id, chat_id: chat1.id)
