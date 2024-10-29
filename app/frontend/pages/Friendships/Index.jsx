f046eecc36810a735109fb8b8f18d664f9c9866e
Dylan Palmboom <nofxboy1234@gmail.com>
2024-10-29 09:54:33 +0200

    Let user remove a friend from the friends index view

--- app/frontend/pages/Friendships/Index.jsx
+++ app/frontend/pages/Friendships/Index.jsx
@@ -1,7 +1,21 @@
-import { Link, Head } from '@inertiajs/react';
+import { Link, Head, router } from '@inertiajs/react';
 import Layout from '../Layout';
+import { useState } from 'react';
 
 export default function Index({ shared, friends }) {
+  const [currentFriends, setCurrentFriends] = useState(friends);
+
+  function handleRemoveFriend(friend) {
+    router.delete(`/friendships/${friend.id}`, {
+      onBefore: (visit) => confirm(`Unfriend ${friend.profile.username}?`),
+      onFinish: (visit) => {
+        setCurrentFriends(
+          currentFriends.filter((user) => user.id != friend.id),
+        );
+      },
+    });
+  }
+
   return (
     <Layout>
       <Head title={'All Friends'} />
@@ -9,10 +23,20 @@ export default function Index({ shared, friends }) {
       <h1>ALL FRIENDS - </h1>
 
       <div>
-        {friends.map((friend) => (
+        {currentFriends.map((friend) => (
           <div key={friend.id}>
             <div>{friend.profile.picture}</div>
             <div>{friend.profile.username}</div>
+            <Link
+              as="button"
+              type="button"
+              onClick={(e) => {
+                e.preventDefault();
+                handleRemoveFriend(friend);
+              }}
+            >
+              Remove Friend
+            </Link>
           </div>
         ))}
       </div>
