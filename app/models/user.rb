55cd4dea86b331e628e4178621482ec7ae51a6c9
Dylan Palmboom <nofxboy1234@gmail.com>
2024-11-08 11:21:14 +0200

    Add :friendships and :friends associations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,32 @@
 class User < ApplicationRecord
+  has_many :friendships,
+  ->(user) {
+   friendships = Friendship.unscope(where: :user_id)
+   query1 = friendships.where(user_id: user.id) # {1}
+   query2 = Friendship.where(friend_id: user.id) # {2}
+   query1.or(query2)
+  },
+  inverse_of: :user,
+  dependent: :destroy
+
+ # SELECT "friendships".*
+ # FROM "friendships"
+ # WHERE ({1}"friendships"."user_id" = 1{1} {2}OR "friendships"."friend_id" = 1{2})
+
+ has_many :friends,
+   ->(user) {
+     User.joins("OR users.id = friendships.user_id") # {3}
+       .where.not(id: user.id) # {4}
+   },
+   through: :friendships
+
+  # SELECT "users".*
+  # FROM "users"
+  # INNER JOIN "friendships"
+  # ON "users"."id" = "friendships"."friend_id" {3}OR users.id = friendships.user_id{3}
+  # WHERE ({1}"friendships"."user_id" = 1{1} {2}OR "friendships"."friend_id" = 1{2})
+  # {4}AND "users"."id" != 1{4}
+
   has_secure_password
 
   generates_token_for :email_verification, expires_in: 2.days do
