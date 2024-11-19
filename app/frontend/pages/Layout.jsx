8403a7eb606ea217a0fb647b1735d4cb4e027754
Dylan Palmboom <nofxboy1234@gmail.com>
2024-11-19 13:10:35 +0200

    Update link hrefs to use path helpers

--- app/frontend/pages/Layout.jsx
+++ app/frontend/pages/Layout.jsx
@@ -13,37 +13,29 @@ const Layout = ({ className, children }) => {
         <Link href="/">Home</Link>
         <Link
           href={api.sessions.destroy.path(shared.session)}
-          // href={`/sessions/${shared.session.id}`}
           as="button"
           type="button"
           method="delete"
         >
           Log out
         </Link>
-        <Link href={`/profiles/${shared.profile.id}`}>
+        <Link href={api.profiles.show.path(shared.profile)}>
           Profile ({shared.current_user.email.split('@')[0]})
         </Link>
         {' | '}
         Friends
         {' | '}
-        <Link href="/friendships/accepted">Online</Link>
-        {' | '}
+        {/* <Link href="/friendships/accepted">Online</Link>
+        {' | '} */}
         <Link href={api.friends.index.path()}>All</Link>
         {' | '}
-        <Link href="/friendships/pending">Pending</Link>
-        {/* <Link href="/chats/new">New chat</Link>
-
-        <h1>Chats</h1>
-        <div>
-          {shared.chats.map((chat) => (
-            <div key={chat.id}>
-              <div>{chat.name}</div>
-              <p>
-                <Link href={`/chats/${chat.id}`}>Show this chat</Link>
-              </p>
-            </div>
-          ))}
-        </div> */}
+        <Link href={api.outgoingFriends.index.path()}>
+          Outgoing Friend Requests
+        </Link>
+        {' | '}
+        <Link href={api.incomingFriends.index.path()}>
+          Incoming Friend Requests
+        </Link>
       </div>
       <br></br>
       <div className={'content'}>
