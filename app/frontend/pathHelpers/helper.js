e437163fa4499f5708695f53269d70c300a96f5b
Dylan Palmboom <nofxboy1234@gmail.com>
2024-11-19 09:19:15 +0200

    Refactor path helper

--- app/frontend/pathHelpers/helper.js
+++ app/frontend/pathHelpers/helper.js
@@ -1,11 +1,7 @@
 import { router } from '@inertiajs/react';
 
 export default function helper({ apiHelper }) {
-  function path(obj) {
-    let interpolatedPath = apiHelper.path(obj);
-
-    return interpolatedPath;
-  }
+  const path = (obj) => apiHelper.path(obj);
 
   const helper = ({ id }, { data = {} }) => {
     router.visit(path({ id }), {
