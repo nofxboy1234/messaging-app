94595b210a146eebc5155df43a7e060aca8447c3
Dylan Palmboom <nofxboy1234@gmail.com>
2024-11-20 10:56:22 +0200

    Remove unused code in Home view

--- app/frontend/pages/home/Index.jsx
+++ app/frontend/pages/home/Index.jsx
@@ -1,94 +1,9 @@
-import Layout from '../Layout';
-import { useEffect, useState } from 'react';
-import { createConsumer } from '@rails/actioncable';
-import MessageDisplay from '../MessageDisplay/MessageDisplay';
-import styles from './Index.module.css';
 import FriendIndex from '../Friend/Index';
 
 function Home({ shared }) {
-  const [values, setValues] = useState({
-    message: '',
-  });
-  const [messages, setMessages] = useState([]);
-  const [messageChannel, setMessageChannel] = useState(null);
-
-  useEffect(() => {
-    console.log('*** Home useEffect');
-    const consumer = createConsumer();
-    const channel = consumer.subscriptions.create('MessageChannel', {
-      connected() {
-        console.log('*** frontend message channel connected');
-      },
-
-      disconnected() {
-        console.log('*** frontend message channel disconnected');
-      },
-
-      received(message) {
-        console.log('*** frontend message channel received');
-
-        setMessages((messages) => [...messages, message]);
-      },
-    });
-
-    setMessageChannel(channel);
-
-    return () => {
-      console.log('*** Home useEffect cleanup');
-      channel.unsubscribe();
-      consumer.disconnect();
-    };
-  }, []);
-
-  function handleChange(e) {
-    const key = e.target.id;
-    const value = e.target.value;
-    setValues((values) => ({
-      ...values,
-      [key]: value,
-    }));
-  }
-
-  function clearMessage() {
-    setValues((values) => ({
-      ...values,
-      message: '',
-    }));
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    if (values.message === '') return;
-    const data = {
-      message: { user: shared.current_user, body: values.message },
-    };
-    messageChannel.send(data);
-    clearMessage();
-  }
-
   console.log('*** Home rendering');
 
-  return (
-    <FriendIndex friends={shared.friends} />
-    // <Layout>
-    //   <div>Home</div>
-
-    //   <MessageDisplay messages={messages} />
-
-    //   <form onSubmit={handleSubmit}>
-    //     <label htmlFor="message">Message:</label>
-    //     <input
-    //       className={styles.messageInput}
-    //       type="text"
-    //       id="message"
-    //       autoFocus
-    //       value={values.message}
-    //       onChange={handleChange}
-    //     />
-    //     <button type="submit">Send</button>
-    //   </form>
-    // </Layout>
-  );
+  return <FriendIndex friends={shared.friends} />;
 }
 
 export default Home;
