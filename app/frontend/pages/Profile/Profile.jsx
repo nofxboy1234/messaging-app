0d330b17031f969abc513872390a583011b55aba
Dylan Palmboom <nofxboy1234@gmail.com>
2024-12-05 09:12:52 +0200

    Move function to check if viewing current user profile to Profile/Show

--- app/frontend/pages/Profile/Profile.jsx
+++ app/frontend/pages/Profile/Profile.jsx
@@ -1,31 +1,19 @@
 import Picture from './Picture';
-import api from '../../pathHelpers';
-import { Link } from '@inertiajs/react';
 import PropTypes from 'prop-types';
 
-function Profile({ currentUser, profile }) {
-  function currentUserProfile() {
-    return currentUser.id === profile.user.id;
-  }
-
+function Profile({ profile }) {
   console.log('render Profile/Profile');
 
   return (
     <div>
-      <div>
-        <Picture src={profile.picture} />
-        <div>{profile.username}</div>
-        <div>{profile.about}</div>
-      </div>
-      {currentUserProfile() ? (
-        <Link href={api.profiles.edit.path(profile)}>Edit this profile</Link>
-      ) : null}
+      <Picture src={profile.picture} />
+      <div>{profile.username}</div>
+      <div>{profile.about}</div>
     </div>
   );
 }
 
 Profile.propTypes = {
-  currentUser: PropTypes.object,
   profile: PropTypes.object,
 };
 
--- app/frontend/pages/Profile/Show.jsx
+++ app/frontend/pages/Profile/Show.jsx
@@ -2,19 +2,29 @@ import { usePage } from '@inertiajs/react';
 import Profile from './Profile';
 import UserActions from '../User/Actions';
 import PropTypes from 'prop-types';
+import { Link } from '@inertiajs/react';
+import api from '../../pathHelpers';
 
 function ProfileShow({ profile, initialRelationship }) {
   const { shared } = usePage().props;
+
+  function currentUserProfile() {
+    return shared.current_user.id === profile.user.id;
+  }
 
   console.log('render Profile/Show');
 
   return (
     <div>
-      <Profile currentUser={shared.current_user} profile={profile} />
-      <UserActions
-        user={profile.user}
-        initialRelationship={initialRelationship}
-      />
+      <Profile profile={profile} />
+      {currentUserProfile() ? (
+        <Link href={api.profiles.edit.path(profile)}>Edit this profile</Link>
+      ) : (
+        <UserActions
+          user={profile.user}
+          initialRelationship={initialRelationship}
+        />
+      )}
     </div>
   );
 }
