7091825948be34d75a536ba557a09ecb9ccb5da1
Dylan Palmboom <nofxboy1234@gmail.com>
2024-12-06 10:08:47 +0200

    Remove unused client-side routes from pathHelpers

--- app/frontend/pathHelpers/all.js
+++ app/frontend/pathHelpers/all.js
@@ -1,10 +1,8 @@
+export { default as outgoingFriends } from './outgoingFriends';
+export { default as incomingFriends } from './incomingFriends';
 export { default as chats } from './chats';
 export { default as friends } from './friends';
-export { default as incomingFriends } from './incomingFriends';
-export { default as memberLists } from './memberLists';
 export { default as messages } from './messages';
-export { default as outgoingFriends } from './outgoingFriends';
 export { default as profiles } from './profiles';
-export { default as users } from './users';
 export { default as sessions } from './sessions';
 export { default as registrations } from './registrations';
