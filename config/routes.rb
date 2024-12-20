494f61717a30edebe55f50a141deb9661cd8f92a
Dylan Palmboom <nofxboy1234@gmail.com>
2024-12-20 12:05:26 +0200

    Generate broadcasts controller

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   resources :member_lists, only: [ :index, :create ], export: true
   resources :messages, only: [ :create, :index ], export: true
   resources :profiles, only: [ :edit, :show, :update ], export: true
+  resources :broadcasts, only: [ :create ], export: true
 
   get  "sign_in", to: "sessions#new"
   post "sign_in", to: "sessions#create", export: true
