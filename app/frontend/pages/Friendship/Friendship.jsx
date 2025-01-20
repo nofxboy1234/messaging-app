9e7c288b87f74108570ec41e28e66ba981d23b8b
Dylan Palmboom <nofxboy1234@gmail.com>
2025-01-20 10:44:37 +0200

    Only show Friendship buttons on hover

--- app/frontend/pages/Friendship/Friendship.jsx
+++ app/frontend/pages/Friendship/Friendship.jsx
@@ -7,7 +7,7 @@ import ProfileLink from '../Profile/Link';
 function Friendship({ className, friendship, user, chat }) {
   return (
     <ProfileLink className={className} user={user}>
-      <div>
+      <div id="buttons">
         <ChatButton chat={chat} />
         <UnfriendButton friendship={friendship} user={user} />
       </div>
@@ -19,6 +19,16 @@ const StyledFriendship = styled(Friendship)`
   background-color: #5fffaf;
   padding: 5rem 3rem 1rem;
   font-size: 2rem;
+
+  > div#buttons {
+    visibility: hidden;
+  }
+
+  &:hover {
+    > div#buttons {
+      visibility: visible;
+    }
+  }
 `;
 
 Friendship.propTypes = {
