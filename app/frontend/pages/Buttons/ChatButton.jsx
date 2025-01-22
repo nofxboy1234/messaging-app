09df2f3347f302fa7dd4d1c80db8ed81e28f37ae
Dylan Palmboom <nofxboy1234@gmail.com>
2025-01-22 12:55:23 +0200

    Create base Button styled-component

---
+++ app/frontend/pages/Buttons/ChatButton.jsx
@@ -0,0 +1,44 @@
+import { Link } from '@inertiajs/react';
+import api from '../../../pathHelpers';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+function Button({ className, chat }) {
+  function handleChat(e) {
+    e.preventDefault();
+
+    api.chats.show({ obj: chat });
+  }
+
+  return (
+    <Link className={className} as="button" type="button" onClick={handleChat}>
+      Chat
+    </Link>
+  );
+}
+
+Button.propTypes = {
+  className: PropTypes.string,
+  chat: PropTypes.object,
+};
+
+const StyledButton = styled(Button)`
+  --bg-color: white;
+  --fg-color: #bf4f74;
+
+  background-color: var(--bg-color);
+  color: var(--fg-color);
+  font-size: 1rem;
+  padding: 0.25rem 1rem;
+  border: 2px solid var(--fg-color);
+  border-radius: 3px;
+  cursor: pointer;
+  margin: 1rem;
+
+  &:hover {
+    background-color: var(--fg-color);
+    color: var(--bg-color);
+  }
+`;
+
+export default StyledButton;
