9e86facbda9eb556075d6f2b7b6e7540a1b34cd9
Dylan Palmboom <nofxboy1234@gmail.com>
2025-01-22 14:32:25 +0200

    Refactor UnfriendButton to be based on StyledButton

--- app/frontend/pages/Friendship/Buttons/UnfriendButton.jsx
+++ app/frontend/pages/Friendship/Buttons/UnfriendButton.jsx
@@ -1,34 +1,36 @@
-import { Link } from '@inertiajs/react';
 import api from '../../../pathHelpers';
+import styled from 'styled-components';
+import Button from '../../Buttons/Button';
 import PropTypes from 'prop-types';
 
-function UnfriendButton({ friendship, user }) {
-  function handleUnfriend(e) {
-    e.preventDefault();
-
-    const options = {
-      onBefore: () => confirm(`Unfriend ${user.profile.username}?`),
-      onFinish: () => {
-        api.unfriendBroadcast.create({ data: friendship });
-      },
-    };
-
-    api.friendships.destroy({
-      obj: friendship,
-      options: options,
-    });
-  }
-
+function UnfriendButton({ className, friendship, user }) {
   return (
-    <Link as="button" type="button" onClick={handleUnfriend}>
-      Unfriend
-    </Link>
+    <Button
+      className={className}
+      text={'Unfriend'}
+      onClick={() => {
+        const options = {
+          onBefore: () => confirm(`Unfriend ${user.profile.username}?`),
+          onFinish: () => {
+            api.unfriendBroadcast.create({ data: friendship });
+          },
+        };
+
+        api.friendships.destroy({
+          obj: friendship,
+          options: options,
+        });
+      }}
+    />
   );
 }
 
 UnfriendButton.propTypes = {
+  className: PropTypes.string,
   friendship: PropTypes.object,
   user: PropTypes.object,
 };
 
-export default UnfriendButton;
+const StyledUnfriendButton = styled(UnfriendButton)``;
+
+export default StyledUnfriendButton;
