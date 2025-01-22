40b17e3e147151085574936a19f1fa33d1777302
Dylan Palmboom <nofxboy1234@gmail.com>
2025-01-22 12:40:07 +0200

    Add css to StyledChatButton

--- app/frontend/pages/Friendship/Buttons/ChatButton.jsx
+++ app/frontend/pages/Friendship/Buttons/ChatButton.jsx
@@ -1,8 +1,9 @@
 import { Link } from '@inertiajs/react';
 import api from '../../../pathHelpers';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-function ChatButton({ chat }) {
+function ChatButton({ className, chat }) {
   function handleChat(e) {
     e.preventDefault();
 
@@ -10,7 +11,7 @@ function ChatButton({ chat }) {
   }
 
   return (
-    <Link as="button" type="button" onClick={handleChat}>
+    <Link className={className} as="button" type="button" onClick={handleChat}>
       Chat
     </Link>
   );
@@ -20,4 +21,22 @@ ChatButton.propTypes = {
   chat: PropTypes.object,
 };
 
-export default ChatButton;
+const StyledChatButton = styled(ChatButton)`
+  --bg-color: white;
+  --fg-color: #bf4f74;
+
+  background-color: var(--bg-color);
+  color: var(--fg-color);
+  font-size: 1rem;
+  padding: 0.25rem 1rem;
+  border: 2px solid var(--fg-color);
+  border-radius: 3px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: var(--fg-color);
+    color: var(--bg-color);
+  }
+`;
+
+export default StyledChatButton;
