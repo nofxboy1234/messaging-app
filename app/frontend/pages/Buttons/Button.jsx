c77161492a21a722e62989d15508d377a7a773e4
Dylan Palmboom <nofxboy1234@gmail.com>
2025-01-22 13:39:37 +0200

    Refactor StyledChatButton to be based on Button
    Override `text` and `onClick` props to be passed to Button.
    This specializes the Button to be a ChatButton.
    The client of ChatButton just needs to pass in the `chat` prop so that
    StyledChatButton can use it in the onClick callback

--- app/frontend/pages/Buttons/Button.jsx
+++ app/frontend/pages/Buttons/Button.jsx
@@ -5,7 +5,6 @@ import styled from 'styled-components';
 function Button({ className, text, onClick }) {
   function handleClick(e) {
     e.preventDefault();
-
     onClick();
   }
 
--- app/frontend/pages/Friendship/Buttons/ChatButton.jsx
+++ app/frontend/pages/Friendship/Buttons/ChatButton.jsx
@@ -1,44 +1,30 @@
-import { Link } from '@inertiajs/react';
 import api from '../../../pathHelpers';
-import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Button from '../../Buttons/Button';
 
-function ChatButton({ className, chat }) {
-  function handleChat(e) {
-    e.preventDefault();
+// function ChatButton({ className, chat }) {
+//   function handleChat(e) {
+//     e.preventDefault();
+//     api.chats.show({ obj: chat });
+//   }
 
-    api.chats.show({ obj: chat });
-  }
+//   return (
+//     <Link className={className} as="button" type="button" onClick={handleChat}>
+//       Chat
+//     </Link>
+//   );
+// }
 
-  return (
-    <Link className={className} as="button" type="button" onClick={handleChat}>
-      Chat
-    </Link>
-  );
-}
+// ChatButton.propTypes = {
+//   className: PropTypes.string,
+//   chat: PropTypes.object,
+// };
 
-ChatButton.propTypes = {
-  className: PropTypes.string,
-  chat: PropTypes.object,
-};
+const StyledChatButton = styled(Button).attrs((props) => ({
+  text: 'Chat',
+  onClick: () => api.chats.show({ obj: props.chat }),
+}))``;
 
-const StyledChatButton = styled(ChatButton)`
-  --bg-color: white;
-  --fg-color: #bf4f74;
-
-  background-color: var(--bg-color);
-  color: var(--fg-color);
-  font-size: 1rem;
-  padding: 0.25rem 1rem;
-  border: 2px solid var(--fg-color);
-  border-radius: 3px;
-  cursor: pointer;
-  margin: 1rem;
-
-  &:hover {
-    background-color: var(--fg-color);
-    color: var(--bg-color);
-  }
-`;
+// const StyleChatButton = <StyledChatButton></StyledChatButton>;
 
 export default StyledChatButton;
