3a1e94953ac9d21781cb5ac2c1f32d9d1b1e9cda
Dylan Palmboom <nofxboy1234@gmail.com>
2025-01-23 09:22:33 +0200

    Refactor AcceptFriendRequestButton to be based on StyledButton

--- app/frontend/pages/FriendRequest/Buttons/AcceptFriendRequestButton.jsx
+++ app/frontend/pages/FriendRequest/Buttons/AcceptFriendRequestButton.jsx
@@ -1,39 +1,43 @@
-import { Link } from '@inertiajs/react';
 import api from '../../../pathHelpers';
+import styled from 'styled-components';
+import Button from '../../Buttons/Button';
 import PropTypes from 'prop-types';
 
-function AcceptFriendRequestButton({ friendRequest }) {
-  function handleAccept(e) {
-    e.preventDefault();
-
-    const options = {
-      onBefore: () =>
-        confirm(
-          `Accept friend request from ${friendRequest.user.profile.username}?`,
-        ),
-      onFinish: () => {
+function AcceptFriendRequestButton({ className, friendRequest }) {
+  return (
+    <Button
+      className={className}
+      text={'Accept Friend Request'}
+      onClick={() => {
         const options = {
+          onBefore: () =>
+            confirm(
+              `Accept friend request from ${friendRequest.user.profile.username}?`,
+            ),
           onFinish: () => {
-            api.acceptFriendRequestBroadcast.create({ data: friendRequest });
+            const options = {
+              onFinish: () => {
+                api.acceptFriendRequestBroadcast.create({
+                  data: friendRequest,
+                });
+              },
+            };
+
+            api.friendships.create({ data: friendRequest, options: options });
           },
         };
 
-        api.friendships.create({ data: friendRequest, options: options });
-      },
-    };
-
-    api.friendRequests.destroy({ obj: friendRequest, options: options });
-  }
-
-  return (
-    <Link as="button" type="button" onClick={handleAccept}>
-      Accept Friend Request
-    </Link>
+        api.friendRequests.destroy({ obj: friendRequest, options: options });
+      }}
+    />
   );
 }
 
 AcceptFriendRequestButton.propTypes = {
+  className: PropTypes.string,
   friendRequest: PropTypes.object,
 };
 
-export default AcceptFriendRequestButton;
+const StyledAcceptFriendRequestButton = styled(AcceptFriendRequestButton)``;
+
+export default StyledAcceptFriendRequestButton;
