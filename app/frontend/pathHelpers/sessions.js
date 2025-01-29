5ef1d5ae6fe39d78f7df999af7608027a358fd10
Dylan Palmboom <nofxboy1234@gmail.com>
2025-01-29 12:18:39 +0200

    Update frontend path helpers

--- app/frontend/pathHelpers/sessions.js
+++ app/frontend/pathHelpers/sessions.js
@@ -2,20 +2,24 @@ import apiHelpers from '../api';
 import helper from './helper';
 
 export default {
-  index: helper({
-    apiHelper: apiHelpers.sessions.index,
-    model: 'session',
-  }),
-  show: helper({
-    apiHelper: apiHelpers.sessions.show,
-    model: 'session',
-  }),
+  // index: helper({
+  //   apiHelper: apiHelpers.sessions.index,
+  //   model: 'session',
+  // }),
+  // show: helper({
+  //   apiHelper: apiHelpers.sessions.show,
+  //   model: 'session',
+  // }),
   destroy: helper({
     apiHelper: apiHelpers.sessions.destroy,
     model: 'session',
   }),
   create: helper({
     apiHelper: apiHelpers.sessions.create,
+    model: 'session',
+  }),
+  new: helper({
+    apiHelper: apiHelpers.sessions.new,
     model: 'session',
   }),
 };
