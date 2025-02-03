ff3b666b720b3059442a842aed79a6587662968b
Dylan Palmboom <nofxboy1234@gmail.com>
2025-02-03 09:33:23 +0200

    Update profile layout

--- app/frontend/pages/Profile/Profile.jsx
+++ app/frontend/pages/Profile/Profile.jsx
@@ -30,10 +30,12 @@ function Profile({ className, initialProfile }) {
   return (
     <div className={className}>
       <div id="header">
-        <Picture className="profile-data" src={profile.picture} />
+        <Picture className="profile-data" src={profile.picture} scale={2} />
         <div className="profile-data">{profile.username}</div>
       </div>
-      <div className="profile-data">About Me: {profile.about}</div>
+      <div className="profile-data" id="about-me">
+        About Me: {profile.about}
+      </div>
     </div>
   );
 }
@@ -46,10 +48,17 @@ const StyledProfile = styled(Profile)`
   & #header {
     display: flex;
     flex-direction: column;
+    gap: 0.5rem;
   }
 
   & .profile-data {
     min-height: 21px;
+  }
+
+  & #about-me {
+    background-color: var(--bg-color-hover);
+    padding: 1rem;
+    border-radius: 8px;
   }
 `;
 
