93ebc8a58d9caa7cbb4adb7ee692963263693591
Dylan Palmboom <nofxboy1234@gmail.com>
2025-02-04 11:12:09 +0200

    Update css of profile edit form

--- app/frontend/pages/Profile/Edit.jsx
+++ app/frontend/pages/Profile/Edit.jsx
@@ -1,30 +1,64 @@
 import ProfileForm from './Form';
 import api from '../../pathHelpers';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-function ProfileEdit({ profile }) {
+function ProfileEdit({ className, profile }) {
   return (
-    <div>
-      <ProfileForm
-        profile={profile}
-        onSubmit={(form) => {
-          const options = {
-            onFinish: () => {
-              api.profileBroadcast.create({ data: profile });
-            },
-          };
+    <div id="profile-edit" className={className}>
+      <div id="profile-container">
+        <ProfileForm
+          profile={profile}
+          onSubmit={(form) => {
+            const options = {
+              onFinish: () => {
+                api.profileBroadcast.create({ data: profile });
+              },
+            };
 
-          form.transform((data) => ({ profile: data }));
-          form.patch(api.profiles.update.path(profile), options);
-        }}
-        submitText="Update profile"
-      />
+            form.transform((data) => ({ profile: data }));
+            form.patch(api.profiles.update.path(profile), options);
+          }}
+          submitText="Update profile"
+        />
+      </div>
+      <div id="spacer"></div>
     </div>
   );
 }
 
 ProfileEdit.propTypes = {
+  className: PropTypes.string,
   profile: PropTypes.object,
 };
 
-export default ProfileEdit;
+const StyledProfileEdit = styled(ProfileEdit)`
+  flex: 1 1 auto;
+  display: flex;
+  flex-direction: column;
+  max-width: 600px;
+  margin-top: 1rem;
+  padding: 1rem;
+
+  & #profile-container {
+    flex: 1 1 0;
+    display: flex;
+    flex-direction: column;
+  }
+
+  /* & #profile-form-container {
+    flex: 1 1 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    border: 1px solid var(--border-color);
+    border-radius: 5px;
+    padding: 1rem;
+  } */
+
+  & #spacer {
+    flex: 1 1 0;
+  }
+`;
+
+export default StyledProfileEdit;
--- app/frontend/pages/Profile/Form.jsx
+++ app/frontend/pages/Profile/Form.jsx
@@ -2,8 +2,10 @@ import { useForm } from '@inertiajs/react';
 import PropTypes from 'prop-types';
 import UpdateProfileButton from './Buttons/UpdateProfileButton';
 import ShowProfileButton from './Buttons/ShowProfileButton';
+import Picture from './Picture';
+import styled from 'styled-components';
 
-function ProfileForm({ profile, onSubmit, submitText }) {
+function ProfileForm({ className, profile, onSubmit }) {
   const form = useForm({
     username: profile.username || '',
     about: profile.about || '',
@@ -11,37 +13,41 @@ function ProfileForm({ profile, onSubmit, submitText }) {
   const { data, setData, errors, processing } = form;
 
   return (
-    <form>
+    <form className={className} id="profile-form-container">
       <div>
-        <label style={{ display: 'block' }} htmlFor="username">
-          Username
-        </label>
-        <input
-          type="text"
-          name="username"
-          id="username"
-          value={data.username}
-          onChange={(e) => setData('username', e.target.value)}
-        />
-        {errors.username && (
-          <div style={{ color: 'red' }}>{errors.username.join(', ')}</div>
-        )}
+        <Picture className="profile-data" src={profile.picture} scale={2} />
+        <div>
+          <div id="username-container">
+            <input
+              type="text"
+              name="username"
+              id="username"
+              value={data.username}
+              onChange={(e) => setData('username', e.target.value)}
+            />
+          </div>
+          {errors.username && (
+            <div style={{ color: 'red' }}>{errors.username.join(', ')}</div>
+          )}
+        </div>
+        <div id="about-me-label">
+          <label style={{ display: 'block' }} htmlFor="about">
+            About Me:
+          </label>
+          <div id="about-container">
+            <textarea
+              name="about"
+              id="about"
+              value={data.about}
+              onChange={(e) => setData('about', e.target.value)}
+            />
+          </div>
+          {errors.about && (
+            <div style={{ color: 'red' }}>{errors.about.join(', ')}</div>
+          )}
+        </div>
       </div>
-      <div>
-        <label style={{ display: 'block' }} htmlFor="about">
-          About
-        </label>
-        <textarea
-          name="about"
-          id="about"
-          value={data.about}
-          onChange={(e) => setData('about', e.target.value)}
-        />
-        {errors.about && (
-          <div style={{ color: 'red' }}>{errors.about.join(', ')}</div>
-        )}
-      </div>
-      <div>
+      <div id="edit-buttons">
         <UpdateProfileButton onSubmit={onSubmit} form={form} />
         <ShowProfileButton profile={profile} />
       </div>
@@ -50,9 +56,66 @@ function ProfileForm({ profile, onSubmit, submitText }) {
 }
 
 ProfileForm.propTypes = {
+  className: PropTypes.string,
   profile: PropTypes.object,
   onSubmit: PropTypes.func,
-  submitText: PropTypes.string,
 };
 
-export default ProfileForm;
+const StyledProfileForm = styled(ProfileForm)`
+  flex: 1 1 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border: 1px solid var(--border-color);
+  border-radius: 5px;
+  padding: 1rem;
+
+  input,
+  textarea {
+    flex: 1 1 0;
+
+    font-family: 'JetbrainsMono', monospace;
+    font-size: 1rem;
+
+    border: 1px solid #e4e4e4;
+    color: var(--medium-grey);
+
+    /* padding: 1rem; */
+    /* min-width: 0; */
+    width: 0;
+  }
+
+  & #username-container {
+    display: flex;
+  }
+
+  & #username {
+    flex: 1 1 0;
+  }
+
+  & #about-me-label {
+    margin-top: 1rem;
+  }
+
+  & #about-container {
+    display: flex;
+  }
+
+  & #about {
+    flex: 1 1 0;
+    display: flex;
+
+    border-radius: 8px;
+    min-height: 63px;
+    overflow-wrap: anywhere;
+  }
+
+  & #edit-buttons {
+    display: flex;
+    gap: 1rem;
+    flex-wrap: wrap;
+    margin-top: 1rem;
+  }
+`;
+
+export default StyledProfileForm;
