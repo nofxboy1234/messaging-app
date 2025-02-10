8781a4dd694b46b9f38f77f3634e1b06da1da30a
Dylan Palmboom <nofxboy1234@gmail.com>
2025-02-10 09:47:11 +0200

    Remove columns from :users table that were added by authentication-zero

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_10_073605) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_10_073950) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -71,12 +71,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_10_073605) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
-    t.string "password_digest", null: false
-    t.boolean "verified", default: false, null: false
-    t.boolean "otp_required_for_sign_in", default: false, null: false
-    t.string "otp_secret", null: false
-    t.string "provider"
-    t.string "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
