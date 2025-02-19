8ac1974ac8d5b9d3597357c3ba8950f18ae1bf0a
Dylan Palmboom <nofxboy1234@gmail.com>
2025-02-19 12:32:45 +0200

    Add factory definitions for all models

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,62 @@
 FactoryBot.define do
+  factory :chat do
+    name { Faker::Lorem.characters(number: 3) }
+    friendship
+  end
+end
+
+FactoryBot.define do
+  factory :friend_request do
+    user
+    friend
+  end
+end
+
+# FactoryBot.define do
+#   factory :friend do
+#     email { Faker::Internet.email }
+#     password { Faker::Internet.password }
+#   end
+# end
+
+FactoryBot.define do
+  factory :friendship do
+    user
+    friend
+  end
+end
+
+FactoryBot.define do
+  factory :member_list do
+    chat
+    user
+  end
+end
+
+FactoryBot.define do
   factory :message do
     body { Faker::Lorem.sentence }
 
-    # user
-    # chat
+    chat
+    user
+  end
+end
+
+FactoryBot.define do
+  factory :profile do
+    username { Faker::Internet.username }
+    picture { "" }
+    about { "" }
+
+    user
+  end
+end
+
+FactoryBot.define do
+  factory :user do
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
+
+    factory :friend
   end
 end
