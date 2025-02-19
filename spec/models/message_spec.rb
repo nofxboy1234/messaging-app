048a82a7e62dee44fa5cf166d62fbee3ef26d08f
Dylan Palmboom <nofxboy1234@gmail.com>
2025-02-19 11:24:47 +0200

    Add test skeleton for message#serialize

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#serialize' do
+    it 'includes the user that created the message and their profile, in the json' do
+    end
+  end
 end
