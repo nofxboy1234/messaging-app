7a348dc4866925a4dadd0a143d305746d7aee68e
Dylan Palmboom <nofxboy1234@gmail.com>
2025-02-19 12:33:27 +0200

    Implement test for message#serialize

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe Message, type: :model do
   describe '#serialize' do
     it 'includes the user that created the message and their profile, in the json' do
+      message = create(:message, body: 'hello')
+
+      expect(message.body).to eq('hello')
     end
   end
 end
