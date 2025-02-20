61b7f4dbfc721725460f71bd4246c38190e75719
Dylan Palmboom <nofxboy1234@gmail.com>
2025-02-20 10:41:25 +0200

    Refactor test for chat#serialize

--- spec/models/chat_spec.rb
+++ spec/models/chat_spec.rb
@@ -2,25 +2,23 @@ require 'rails_helper'
 
 RSpec.describe Chat, type: :model do
   describe '#serialize' do
-    it 'includes chat.messages in the json' do
-      chat = create(:chat, :with_messages)
-      json = chat.serialize
+    context 'when a chat has 1 message sent by a user with email: user1@example.com, username: user1' do
+      let(:json) do
+        chat = create(:chat, :with_messages)
+        chat.serialize
+      end
 
-      expect(json.dig("messages").class).to eq(Array)
-    end
+      it 'includes chat.messages array with length of 1' do
+        expect(json.dig("messages").class).to eq(Array)
+      end
 
-    it 'includes chat.messages[index].user in the json' do
-      chat = create(:chat, :with_messages)
-      json = chat.serialize
+      it 'includes chat.messages[index].user in the json' do
+        expect(json.dig("messages", 0, "user")).not_to be_nil
+      end
 
-      expect(json.dig("messages", 0, "user")).not_to be_nil
-    end
-
-    it 'includes chat.messages[index].user.profile in the json' do
-      chat = create(:chat, :with_messages)
-      json = chat.serialize
-
-      expect(json.dig("messages", 0, "user", "profile")).not_to be_nil
+      it 'includes chat.messages[index].user.profile in the json' do
+        expect(json.dig("messages", 0, "user", "profile")).not_to be_nil
+      end
     end
   end
 end
