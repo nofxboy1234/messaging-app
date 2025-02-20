34c1c5549dc691075ae531aee6fb8c29cc41c063
Dylan Palmboom <nofxboy1234@gmail.com>
2025-02-20 12:27:35 +0200

    Add test for profile#serialize

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -5,8 +5,25 @@ RSpec.describe Profile, type: :model do
     let(:profile) do
       create(:profile)
     end
+
     it 'returns a gravatar url that points to the user profile picture' do
       expect(profile.picture).to start_with("https://www.gravatar.com/avatar/")
+    end
+  end
+
+  describe '#serialize' do
+    let(:json) do
+      user = create(:user, email: 'user1@example.com', password: '123456')
+      user.profile = create(:profile, username: 'user1')
+      user.profile.serialize
+    end
+
+    it 'includes the user' do
+      expect(json.dig("user", "email")).to eq('user1@example.com')
+    end
+
+    it "includes the users' profile" do
+      expect(json.dig("user", "profile", "username")).to eq('user1')
     end
   end
 end
