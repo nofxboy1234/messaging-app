fab062356169a60a44bc79b2c9e1976f3a6aa0c2
Dylan Palmboom <nofxboy1234@gmail.com>
2025-02-20 12:45:29 +0200

    Refactor test setup for Profile

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,23 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  describe '#picture' do
-    let(:profile) do
-      create(:profile)
-    end
+  let(:profile) do
+    user = create(:user, email: 'user1@example.com', password: '123456')
+    user.profile = create(:profile, username: 'user1')
+  end
 
+  let(:json) do
+    profile.serialize
+  end
+
+  describe '#picture' do
     it 'returns a gravatar url that points to the user profile picture' do
       expect(profile.picture).to start_with("https://www.gravatar.com/avatar/")
     end
   end
 
-  describe '#serialize' do
-    let(:json) do
-      user = create(:user, email: 'user1@example.com', password: '123456')
-      user.profile = create(:profile, username: 'user1')
-      user.profile.serialize
-    end
+  describe '#show_data' do
+  end
 
+  describe '#serialize' do
     it 'includes the user' do
       expect(json.dig("user", "email")).to eq('user1@example.com')
     end
