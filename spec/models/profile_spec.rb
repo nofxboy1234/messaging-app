d24c3bb05fc8c49d37fc45819f3e5e46c35b0c3b
Dylan Palmboom <nofxboy1234@gmail.com>
2025-02-20 12:21:14 +0200

    Add test for profile#picture

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#picture' do
+    let(:profile) do
+      create(:profile)
+    end
+    it 'returns a gravatar url that points to the user profile picture' do
+      expect(profile.picture).to start_with("https://www.gravatar.com/avatar/")
+    end
+  end
 end
