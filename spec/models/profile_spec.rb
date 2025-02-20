0257ede7727726055828e01c8871a0c38d9ee141
Dylan Palmboom <nofxboy1234@gmail.com>
2025-02-20 15:00:30 +0200

    Add test for profile#show_data

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,10 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  let(:profile) do
+  let(:viewing_user) do
     user = create(:user, email: 'user1@example.com', password: '123456')
     user.profile = create(:profile, username: 'user1')
+    return user
   end
+
+  let(:viewed_user) do
+    user = create(:user, email: 'user2@example.com', password: '123456')
+    user.profile = create(:profile, username: 'user2')
+    return user
+  end
+
+  let(:profile) do
+    viewed_user.profile
+  end
+
 
   let(:json) do
     profile.serialize
@@ -17,15 +29,31 @@ RSpec.describe Profile, type: :model do
   end
 
   describe '#show_data' do
+    context 'when the viewing_user is friends with the viewed_user, as the receiver of the friend request' do
+      let(:friendship) do
+        create(:friendship, user: viewed_user, friend: viewing_user)
+      end
+      it 'returns a hash with data to show on a profile, relevant to the viewer, depending on the relationship to the viewed user' do
+        show_data = {
+          profile: profile.serialize,
+          relationship: "friend",
+          friendRequest: nil,
+          friendship: friendship,
+          chat: friendship.chat
+        }
+
+        expect(profile.show_data(viewing_user)).to eq(show_data)
+      end
+    end
   end
 
   describe '#serialize' do
     it 'includes the user' do
-      expect(json.dig("user", "email")).to eq('user1@example.com')
+      expect(json.dig("user", "email")).to eq('user2@example.com')
     end
 
     it "includes the users' profile" do
-      expect(json.dig("user", "profile", "username")).to eq('user1')
+      expect(json.dig("user", "profile", "username")).to eq('user2')
     end
   end
 end
