7ef4ceb4166daa5d277c34f61ac73ba2d6b65292
Dylan Palmboom <nofxboy1234@gmail.com>
2025-02-20 10:17:59 +0200

    Refactor chat#serialize tests

--- spec/models/chat_spec.rb
+++ spec/models/chat_spec.rb
@@ -2,7 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Chat, type: :model do
   describe '#serialize' do
-    it 'includes chat.messages, message.user, user.profile' do
+    it 'includes chat.messages in the json' do
+      chat = create(:chat, :with_messages)
+      json = chat.serialize
+
+      expect(json.dig("messages").class).to eq(Array)
+    end
+
+    it 'includes chat.messages[index].user in the json' do
+      chat = create(:chat, :with_messages)
+      json = chat.serialize
+
+      expect(json.dig("messages", 0, "user")).not_to be_nil
+    end
+
+    it 'includes chat.messages[index].user.profile in the json' do
       chat = create(:chat, :with_messages)
       json = chat.serialize
 
