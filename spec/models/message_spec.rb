3efe31d00f2c937a3a90b4496ba9fe803cf09632
Dylan Palmboom <nofxboy1234@gmail.com>
2025-02-20 11:37:37 +0200

    Refactor message#serialize

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,20 +2,23 @@ require 'rails_helper'
 
 RSpec.describe Message, type: :model do
   describe '#serialize' do
-    it 'includes the user that created the message in the json' do
-      message = create(:message)
-      json = message.serialize
+    context 'when a chat has 2 members, user1 and user2' do
+      let(:json) do
+        user = create(:user, email: 'user1@example.com', password: '123456')
+        user.profile = create(:profile, username: 'user1')
+        message = create(:message, body: 'hello', user:)
+        message.serialize
+      end
 
-      expect(json).to include("user")
-    end
+      context 'when a message is sent by a user with user.email: user1@example.com, profile.username: user1' do
+        it 'includes the user that created the message' do
+          expect(json.dig("user", "email")).to eq('user1@example.com')
+        end
 
-    it 'includes the profile of the user that created the message in the json' do
-      message = create(:message)
-      message.user.profile = create(:profile)
-      json = message.serialize
-      user = json["user"]
-
-      expect(user).to include("profile")
+        it "includes the users' profile" do
+          expect(json.dig("user", "profile", "username")).to eq('user1')
+        end
+      end
     end
   end
 end
