7bb5eb6dfbeba649a1edcfd00d91a6dab2ec67fd
Dylan Palmboom <nofxboy1234@gmail.com>
2025-02-24 11:01:30 +0200

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,56 +8,31 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-# user1 = User.create!(email: 'user1@example.com', password: 'user1!@#$%^&')
-# user2 = User.create!(email: 'user2@example.com', password: 'user2!@#$%^&')
-# user3 = User.create!(email: 'user3@example.com', password: 'user3!@#$%^&')
-# user4 = User.create!(email: 'user4@example.com', password: 'user4!@#$%^&')
-# user5 = User.create!(email: 'user5@example.com', password: 'user5!@#$%^&')
+def create_user_with_profile
+  User.create(email: Faker::Internet.email,
+              password: 123456,
+              profile: Profile.create(username: Faker::Internet.username,
+                                      picture: "",
+                                      about: ""))
+end
 
-# Profile.create!(
-#                 username: user1.email.split('@').first,
-#                 picture: '',
-#                 about: "Hello, I am #{user1.email.split('@').first}!",
-#                 user_id: user1.id
-#                 )
-# Profile.create!(
-#   username: user2.email.split('@').first,
-#   picture: '',
-#   about: "Hello, I am #{user2.email.split('@').first}!",
-#   user_id: user2.id
-# )
+def create_user_without_profile
+  User.create(email: Faker::Internet.email,
+              password: 123456)
+end
 
-#   Profile.create!(
-#   username: user3.email.split('@').first,
-#   picture: '',
-#   about: "Hello, I am #{user3.email.split('@').first}!",
-#   user_id: user3.id
-# )
+user = create_user_with_profile
 
-# Profile.create!(
-#   username: user4.email.split('@').first,
-#   picture: '',
-#   about: "Hello, I am #{user4.email.split('@').first}!",
-#   user_id: user4.id
-# )
+for i in 1..3 do
+  friend = create_user_with_profile
+  friendship = Friendship.create(user:, friend:)
+  chat = Chat.create(name: Faker::Lorem.characters(number: 3))
+  chat.members << [ user, friend ]
+  friendship.chat = chat
+end
 
-# Profile.create!(
-#   username: user5.email.split('@').first,
-#   picture: '',
-#   about: "Hello, I am #{user5.email.split('@').first}!",
-#   user_id: user5.id
-# )
-
-# user1.friends << user2
-
-# current_user = user1
-# @friend = user2
-# @friendship = current_user.friendships.where(user: @friend).or(current_user.friendships.where(friend: @friend)).take
-# chat1 = Chat.create!(
-#   name: "#{current_user.profile.username}_#{@friend.profile.username}",
-#   friendship: @friendship
-# )
-# chat1.members << [ current_user, @friend ]
-
-# Message.create!(body: 'Hello user2!', user_id: user1.id, chat_id: chat1.id)
-# Message.create!(body: 'Hello user1!', user_id: user2.id, chat_id: chat1.id)
+# friend = create_user_without_profile
+# friendship = Friendship.create(user:, friend:)
+# chat = Chat.create(name: Faker::Lorem.characters(number: 3))
+# chat.members << [ user, friend ]
+# friendship.chat = chat
