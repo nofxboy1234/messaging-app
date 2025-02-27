bfffa61aaf9829f0d038ea49a43f160f345b83e4
Dylan Palmboom <nofxboy1234@gmail.com>
2025-02-27 11:15:17 +0200

    Add tests for user validations with Devise

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe '#all_friends' do
-    # let(:friendship) do
+  describe 'users must have valid emails and passwords' do
+    context 'when a user has valid email and password' do
+      it 'the user is saved successfully' do
+        user = create(:user)
 
-    #   friendship
-    # end
+        expect(user).to be_persisted
+      end
+    end
+
+    context 'when a user has no email' do
+      it 'the user is not saved' do
+        user = build(:user, email: nil)
+        user.save
+        expect(user).not_to be_persisted
+      end
+
+      it 'the user has a blank email error' do
+        user = build(:user, email: nil)
+        user.save
+        expect(user.errors.full_messages).to eq([ "Email can't be blank" ])
+      end
+    end
+
+    context 'when a user has an invalid email' do
+      it 'the user is not saved' do
+        user = build(:user, email: '923874728^^sdifu')
+        user.save
+        expect(user).not_to be_persisted
+      end
+
+      it 'the user has an invalid email error' do
+        user = build(:user, email: '923874728^^sdifu')
+        user.save
+        expect(user.errors.full_messages).to eq([ "Email is invalid" ])
+      end
+    end
+
+    context 'when a user has a password that is too short' do
+      it 'the user is not saved' do
+        user = build(:user, password: '123')
+        user.save
+        expect(user).not_to be_persisted
+      end
+
+      it 'the user has a password length error' do
+        user = build(:user, password: '123')
+        user.save
+        expect(user.errors.full_messages).to eq([ "Password is too short (minimum is 6 characters)" ])
+      end
+    end
+  end
+
+  describe '#all_friends' do
     context 'when a user is friends with a direct and indirect friend' do
       it 'returns all direct and inverse friends' do
         user1 = create(:user, email: 'user1@example.com')
