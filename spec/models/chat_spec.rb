b8147e7d91220770e2db9ab7ed0eaca582b99acc
Dylan Palmboom <nofxboy1234@gmail.com>
2025-02-28 09:34:44 +0200

    Add tests for Chat model

--- spec/models/chat_spec.rb
+++ spec/models/chat_spec.rb
@@ -1,6 +1,64 @@
 require 'rails_helper'
 
 RSpec.describe Chat, type: :model do
+  describe 'the messages associated with a chat' do
+    context 'when a chat is destroyed' do
+      it 'the messages are also destroyed' do
+        chat = create(:chat, :with_messages)
+        expect(chat).to be_persisted
+        expect(chat.messages.count).to be > 0
+
+        chat.destroy!
+        expect(chat).not_to be_persisted
+        expect(chat.messages.count).to eq(0)
+      end
+    end
+  end
+
+  describe 'the memberships associated with a chat' do
+    context 'when a chat is destroyed' do
+      it 'the memberships are also destroyed' do
+        user = create(:user, email: 'user1@example.com', password: '123456')
+        user.profile = create(:profile, username: 'user1')
+        user2 = create(:user, email: 'user2@example.com', password: '123456')
+        user2.profile = create(:profile, username: 'user2')
+        chat = create(:chat)
+        chat.members << [ user, user2 ]
+        expect(chat).to be_persisted
+        expect(chat.memberships.count).to eq(2)
+
+        chat.destroy!
+        expect(chat).not_to be_persisted
+        expect(chat.memberships.count).to eq(0)
+      end
+    end
+  end
+
+  describe 'the friendship a chat is associated with' do
+    context 'when a chat is created without a friendship' do
+      it 'the chat is not saved' do
+        chat = build(:chat, friendship: nil)
+        chat.save
+        expect(chat).not_to be_persisted
+      end
+    end
+
+    context 'when a chat is created with a friendship' do
+      it 'the chat is saved' do
+        chat = build(:chat)
+        chat.save
+        expect(chat).to be_persisted
+      end
+
+      it 'chat.friendship returns the friendship' do
+        friendship = create(:friendship)
+        chat = build(:chat, friendship:)
+        chat.save
+        expect(chat.friendship).to be(friendship)
+      end
+    end
+  end
+
   describe '#serialize' do
     context 'when a chat has 2 members, user1 and user2' do
       let(:json) do
