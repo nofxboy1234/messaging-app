6d15e2627158df31df9ec58b215260b8b76aa24e
Dylan Palmboom <nofxboy1234@gmail.com>
2025-02-28 11:00:11 +0200

    Add tests for Message model

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,6 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
+  describe 'the user a message is associated with' do
+    context 'when a message is created without a user' do
+      it 'the message is not saved' do
+        message = build(:message, user: nil)
+        message.save
+        expect(message).not_to be_persisted
+      end
+    end
+
+    context 'when a message is created with a user' do
+      it 'the message is saved' do
+        message = build(:message)
+        message.save
+        expect(message).to be_persisted
+      end
+
+      it 'message.user returns the user' do
+        user = create(:user)
+        message = build(:message, user:)
+        message.save
+        expect(message.user).to be(user)
+      end
+    end
+  end
+
+  describe 'the chat a message is associated with' do
+    context 'when a message is created without a chat' do
+      it 'the message is not saved' do
+        message = build(:message, chat: nil)
+        message.save
+        expect(message).not_to be_persisted
+      end
+    end
+
+    context 'when a message is created with a chat' do
+      it 'the message is saved' do
+        message = build(:message)
+        message.save
+        expect(message).to be_persisted
+      end
+
+      it 'message.chat returns the chat' do
+        chat = create(:chat)
+        message = build(:message, chat:)
+        message.save
+        expect(message.chat).to be(chat)
+      end
+    end
+  end
+
   describe '#serialize' do
     let(:json) do
       user = create(:user, email: 'user1@example.com', password: '123456')
