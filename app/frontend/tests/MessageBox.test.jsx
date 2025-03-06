a4171cf0f5b465ece2019df283de71687675c2db
Dylan Palmboom <nofxboy1234@gmail.com>
2025-03-06 15:31:29 +0200

    Add tests for MessageBox

--- app/frontend/tests/MessageBox.test.jsx
+++ app/frontend/tests/MessageBox.test.jsx
@@ -22,61 +22,56 @@ describe('MessageBox', () => {
 
       expect(sendMessage).toHaveBeenCalledWith('Hello', chat);
     });
-    it.skip('should clear the input', () => {});
-    it.skip('should focus the input', () => {});
+
+    it('should clear the input', async () => {
+      const user = userEvent.setup();
+      const chat = {};
+      render(<MessageBox chat={chat} />);
+
+      const input = screen.getByRole('textbox', { name: '' });
+      const button = screen.getByRole('button', { name: 'Send' });
+      await user.type(input, 'Hello');
+      await user.click(button);
+
+      expect(input).toHaveValue('');
+    });
+
+    it('should focus the input', async () => {
+      const user = userEvent.setup();
+      const chat = {};
+      render(<MessageBox chat={chat} />);
+
+      const input = screen.getByRole('textbox', { name: '' });
+      const button = screen.getByRole('button', { name: 'Send' });
+      await user.click(button);
+
+      expect(input).toHaveFocus();
+    });
   });
 
-  it.skip('should update the typed text when the input is typed into', async () => {
-    const user = userEvent.setup();
-    render(<MessageBox />);
+  describe('when the Enter key is pressed', () => {
+    it('should send a message to the given "chat" in props', async () => {
+      const user = userEvent.setup();
+      const chat = {};
+      render(<MessageBox chat={chat} />);
 
-    const input = screen.getByRole('textbox', { name: '' });
-    await user.type(input, 'Hello');
+      const input = screen.getByRole('textbox', { name: '' });
+      await user.type(input, 'Hey!{Enter}');
 
-    expect(input).toHaveValue('Hello');
+      expect(sendMessage).toHaveBeenCalledWith('Hey!', chat);
+    });
   });
 
-  // ************************************
+  describe('when the input is typed into', () => {
+    it('should update the typed text', async () => {
+      const user = userEvent.setup();
+      const chat = {};
+      render(<MessageBox chat={chat} />);
 
-  it.skip('should render a blank input', () => {
-    render(<MessageBox />);
-    const input = screen.getByRole('textbox', { name: '' });
-    expect(input).toBeInTheDocument();
-  });
+      const input = screen.getByRole('textbox', { name: '' });
+      await user.type(input, 'Hello');
 
-  it.skip('should display the typed text when typed into', async () => {
-    const user = userEvent.setup();
-    render(<MessageBox />);
-
-    const input = screen.getByRole('textbox', { name: '' });
-    await user.type(input, 'Hello');
-
-    expect(input).toHaveValue('Hello');
-  });
-
-  it.skip('should call the onChange function when typed into', async () => {
-    const onChange = vi.fn();
-    const user = userEvent.setup();
-    render(<MessageBox onChange={onChange} />);
-
-    const input = screen.getByRole('textbox', { name: '' });
-    await user.type(input, 'Hello');
-
-    expect(onChange).toHaveBeenCalled();
-  });
-
-  it.skip('should not call the onChange function when not typed into', () => {
-    const onChange = vi.fn();
-    render(<MessageBox onChange={onChange} />);
-
-    expect(onChange).not.toHaveBeenCalled();
-  });
-
-  it.skip('should display the message prop text', async () => {
-    render(<MessageBox message={'Bye'} />);
-
-    const input = screen.getByRole('textbox', { name: '' });
-
-    expect(input).toHaveValue('Bye');
+      expect(input).toHaveValue('Hello');
+    });
   });
 });
