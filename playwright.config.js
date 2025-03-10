7bce5dad56a4da5c133331c9ed901be1fba1980c
Dylan Palmboom <nofxboy1234@gmail.com>
2025-03-10 15:10:51 +0200

    Update Playwright to Ignore .test.js files

--- playwright.config.js
+++ playwright.config.js
@@ -14,6 +14,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './app/frontend/tests',
+  testIgnore: '**/*.test.?(c|m)[jt]s?(x)',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -78,4 +79,3 @@ export default defineConfig({
   //   reuseExistingServer: !process.env.CI,
   // },
 });
-
