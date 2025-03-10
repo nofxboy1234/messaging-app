8eef42669a82c01aa815b6c091b9d295c177f764
Dylan Palmboom <nofxboy1234@gmail.com>
2025-03-10 21:50:16 +0200

    Update npm test script back to what it was
    The Playwright script was not being interpreted as expected, so using vanilla command

--- app/frontend/tests/e2e/Chat.spec.js
+++ app/frontend/tests/e2e/Chat.spec.js
@@ -8,4 +8,11 @@ test.describe('when the scrollbar is at the bottom and a message is received', (
     const heading = page.getByText('Email');
     await expect(heading).toBeVisible();
   });
+
+  test('should show the chat page', async ({ page }) => {
+    await page.goto('/chats/2');
+
+    const button = page.getByRole('button', { name: 'Send' });
+    await expect(button).toBeVisible();
+  });
 });
