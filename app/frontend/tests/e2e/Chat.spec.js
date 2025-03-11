dd75f37cc745f62b6af9dd11e33da0f86125ac15
Dylan Palmboom <nofxboy1234@gmail.com>
2025-03-11 11:13:49 +0200

    Update Chat e2e tests

--- app/frontend/tests/e2e/Chat.spec.js
+++ app/frontend/tests/e2e/Chat.spec.js
@@ -1,20 +1,23 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test.describe('when the scrollbar is at the bottom and a message is received', () => {
+test.describe('when navigating to the Chat page', () => {
   test.beforeEach('Log in', async ({ page }) => {
     await page.goto('/');
     await page.getByLabel('Email:').fill('user1@example.com');
     await page.getByLabel('Password:').fill('123456');
     await page.getByRole('button', { name: 'Log in' }).click();
     await expect(page.getByText('Signed in successfully.')).toBeVisible();
-  });
 
-  test('should show the chat page', async ({ page }) => {
     const chatIndex = page.getByTestId('chat-index');
-    const link = chatIndex.getByRole('link', { name: 'user2' });
+    const link = chatIndex.getByRole('link', { name: 'user5' });
     await link.click();
     const sendButton = page.getByRole('button', { name: 'Send' });
     await expect(sendButton).toBeVisible();
+  });
+
+  test('should show the last message', async ({ page }) => {
+    const lastMessage = page.getByText('last message');
+    await expect(lastMessage).toBeVisible();
   });
 });
