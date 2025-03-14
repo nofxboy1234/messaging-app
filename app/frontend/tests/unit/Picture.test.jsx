4c8ba45fe9b324a80aba862c3adef445b113072f
Dylan Palmboom <nofxboy1234@gmail.com>
2025-03-14 11:15:15 +0200

    Add unit tests for Picture component

---
+++ app/frontend/tests/unit/Picture.test.jsx
@@ -0,0 +1,49 @@
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import Picture from '../../pages/Profile/Picture';
+
+describe('Message', () => {
+  it('should render an image representing a user profile picture', () => {
+    const src = '';
+    const scale = 1;
+    render(<Picture src={src} scale={scale} />);
+
+    const image = screen.getByRole('img', { name: 'Profile picture' });
+    expect(image).toBeInTheDocument();
+  });
+
+  it('should render an image using the src prop', () => {
+    const src = 'https://example.com/pictures/user1';
+    const scale = 1;
+    render(<Picture src={src} scale={scale} />);
+
+    const image = screen.getByRole('img', { name: 'Profile picture' });
+    expect(image.getAttribute('src')).toBe(
+      'https://example.com/pictures/user1',
+    );
+  });
+
+  describe('when the scale is 1', () => {
+    it('should render a 40 x 40 image with width: "40px", height: "40px"', () => {
+      const src = './mock_profile_picture';
+      const scale = 1;
+      render(<Picture src={src} scale={scale} />);
+
+      const image = screen.getByRole('img', { name: 'Profile picture' });
+      expect(image.getAttribute('width')).toBe('40px');
+      expect(image.getAttribute('height')).toBe('40px');
+    });
+  });
+
+  describe('when the scale is 0.5', () => {
+    it('should render a 40 x 40 image with width: "20px", height: "20px"', () => {
+      const src = './mock_profile_picture';
+      const scale = 0.5;
+      render(<Picture src={src} scale={scale} />);
+
+      const image = screen.getByRole('img', { name: 'Profile picture' });
+      expect(image.getAttribute('width')).toBe('20px');
+      expect(image.getAttribute('height')).toBe('20px');
+    });
+  });
+});
