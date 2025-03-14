85ef7504b941279aba6187a8688bc1db8c7889ab
Dylan Palmboom <nofxboy1234@gmail.com>
2025-03-14 15:28:49 +0200

    Mock inertiajs Link component for more test isolation

--- app/frontend/tests/unit/UserLink.test.jsx
+++ app/frontend/tests/unit/UserLink.test.jsx
@@ -1,6 +1,18 @@
-import { describe, it, expect } from 'vitest';
+import { vi, describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import UserLink from '../../pages/User/Link';
+
+vi.mock('@inertiajs/react', () => {
+  console.log('mocking Link');
+
+  return {
+    Link: ({ className, children, href }) => (
+      <a className={className} href={href}>
+        {children}
+      </a>
+    ),
+  };
+});
 
 describe('UserLink', () => {
   it('should render its children into a link', () => {
