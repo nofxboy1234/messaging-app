1a9ef7b4bc20b241c60e4588df94f813a1968cc1
Dylan Palmboom <nofxboy1234@gmail.com>
2025-03-18 13:40:49 +0200

    Update inertia usePage mock
    Make it more closely resemble the actual implementation where it memoizes and keeps usePage().props
    stable across renders.

--- app/frontend/tests/unit/Chat/Show.test.jsx
+++ app/frontend/tests/unit/Chat/Show.test.jsx
@@ -45,19 +45,21 @@ vi.mock('../../../channels/subscriptions', () => {
   };
 });
 
+const props = {
+  shared: {
+    users: [
+      { id: 1, username: 'user1' },
+      { id: 2, username: 'user2' },
+      { id: 3, username: 'user3' },
+      { id: 4, username: 'user4' },
+    ],
+    current_user: { id: 1, username: 'user1' },
+  },
+};
+
 vi.mock('@inertiajs/react', () => ({
   usePage: () => ({
-    props: {
-      shared: {
-        users: [
-          { id: 1, username: 'user1' },
-          { id: 2, username: 'user2' },
-          { id: 3, username: 'user3' },
-          { id: 4, username: 'user4' },
-        ],
-        current_user: { id: 1, username: 'user1' },
-      },
-    },
+    props,
   }),
 }));
 
