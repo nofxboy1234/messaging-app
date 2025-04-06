433cedaf1df10bc23aaf3fcac5ada9b02fa2e0bf
Dylan Palmboom <nofxboy1234@gmail.com>
2025-04-06 09:29:17 +0200

    Remove console.logs

--- app/frontend/pages/User/ChatUserIndex.jsx
+++ app/frontend/pages/User/ChatUserIndex.jsx
@@ -4,29 +4,12 @@ import ProfileLink from '../Profile/Link';
 import styled from 'styled-components';
 import { usePage } from '@inertiajs/react';
 import useSetupChatUsers from '../../hooks/useSetupChatUsers';
-import usePreviousValues from '../../hooks/usePreviousValues';
-import logChangedValues from '../../helpers/logChangedValues';
 
 function ChatUserIndex({ className }) {
-  // check reinitializeUsers state change is 1 render when clicking user3 then user4
-  // instead of 2 renders: context, state
-
   const pageContext = usePage();
   const { chat: activeChat } = pageContext.props;
 
   const users = useSetupChatUsers(activeChat.members, activeChat.id);
-
-  console.log('*** render values ***');
-  console.log('page context:', pageContext);
-  console.log('users:', users);
-  console.log('*********************');
-  console.log('\n');
-
-  console.log('*** changed values ***');
-  const prevValues = usePreviousValues({ 'page context': usePage(), users });
-  logChangedValues(...prevValues);
-  console.log('**********************');
-  console.log('\n');
 
   return (
     <div className={className}>
