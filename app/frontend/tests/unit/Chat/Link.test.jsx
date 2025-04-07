081713f01dd97a77056e2005e988e0b9de52f01a
Dylan Palmboom <nofxboy1234@gmail.com>
2025-04-07 22:20:18 +0200

    Update ChatLink tests and mocks

--- app/frontend/tests/unit/Chat/Link.test.jsx
+++ app/frontend/tests/unit/Chat/Link.test.jsx
@@ -3,8 +3,8 @@ import { render, screen } from '@testing-library/react';
 import ChatLink from '../../../pages/Chat/Link';
 
 vi.mock('../../../pathHelpers', () => {
-  const show = vi.fn();
-  show.path = vi.fn();
+  const show = () => {};
+  show.path = (chat) => `/path/to/chat${chat.id}`;
 
   return {
     default: { chats: { show } },
@@ -12,49 +12,66 @@ vi.mock('../../../pathHelpers', () => {
 });
 
 vi.mock('../../../pages/User/Link', () => ({
-  default: ({ children, targetPath }) => <div>user link {children}</div>,
+  default: ({ children, targetPath }) => (
+    <div>
+      {targetPath}
+      {children}
+    </div>
+  ),
 }));
 
 vi.mock('../../../pages/Profile/Picture', () => {
   return {
-    default: function Picture({ src, scale }) {
-      return <div>profile picture</div>;
+    default: function Picture({ src }) {
+      return <div>{src}</div>;
     },
   };
 });
 
 describe('ChatLink', () => {
-  it.only('should render a user link', () => {
-    const chat = {};
+  it('renders a link with the correct chat path', () => {
+    const chat = { id: 1 };
     const friend = {
-      profile: { username: 'friend1', picture: 'path/to/picture' },
+      profile: { username: 'friend1', picture: '/path/to/picture' },
     };
     render(<ChatLink chat={chat} friend={friend} />);
 
-    const userLink = screen.getByText('user link');
-
-    expect(userLink).toBeInTheDocument();
+    screen.debug();
+    const link = screen.getByText('/path/to/chat1');
+    expect(link).toBeInTheDocument();
   });
 
-  it('should render any children passed to it', () => {
-    const user = { profile: { picture: 'picture url', username: 'user1' } };
-    const active = true;
-    const scale = 1;
-    render(
-      <ChatLink user={user} active={active} scale={scale}>
-        <div>child 1</div>
-        <div>child 2</div>
-        <div>
-          <div>child 3</div>
-        </div>
-      </ChatLink>,
-    );
+  it('renders an image with the correct profile picture source', () => {
+    const chat = { id: 1 };
+    const friend = {
+      profile: { username: 'friend1', picture: '/path/to/picture' },
+    };
+    render(<ChatLink chat={chat} friend={friend} />);
 
-    const child1 = screen.getByText('child 1');
-    expect(child1).toBeInTheDocument();
-    const child2 = screen.getByText('child 2');
-    expect(child2).toBeInTheDocument();
-    const child3 = screen.getByText('child 3');
-    expect(child3).toBeInTheDocument();
+    screen.debug();
+    const image = screen.getByText('/path/to/picture');
+    expect(image).toBeInTheDocument();
   });
+
+  // it('should render any children passed to it', () => {
+  //   const user = { profile: { picture: 'picture url', username: 'user1' } };
+  //   const active = true;
+  //   const scale = 1;
+  //   render(
+  //     <ChatLink user={user} active={active} scale={scale}>
+  //       <div>child 1</div>
+  //       <div>child 2</div>
+  //       <div>
+  //         <div>child 3</div>
+  //       </div>
+  //     </ChatLink>,
+  //   );
+
+  //   const child1 = screen.getByText('child 1');
+  //   expect(child1).toBeInTheDocument();
+  //   const child2 = screen.getByText('child 2');
+  //   expect(child2).toBeInTheDocument();
+  //   const child3 = screen.getByText('child 3');
+  //   expect(child3).toBeInTheDocument();
+  // });
 });
