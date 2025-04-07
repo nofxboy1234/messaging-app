60547c5d1d01a8d5fe35ca7ca5684b4351b09851
Dylan Palmboom <nofxboy1234@gmail.com>
2025-04-07 22:35:24 +0200

    Update ChatLink tests

--- app/frontend/tests/unit/Chat/Link.test.jsx
+++ app/frontend/tests/unit/Chat/Link.test.jsx
@@ -29,49 +29,51 @@ vi.mock('../../../pages/Profile/Picture', () => {
 });
 
 describe('ChatLink', () => {
-  it('renders a link with the correct chat path', () => {
+  it('should pass the correct chat path', () => {
     const chat = { id: 1 };
     const friend = {
       profile: { username: 'friend1', picture: '/path/to/picture' },
     };
     render(<ChatLink chat={chat} friend={friend} />);
 
-    screen.debug();
     const link = screen.getByText('/path/to/chat1');
     expect(link).toBeInTheDocument();
   });
 
-  it('renders an image with the correct profile picture source', () => {
+  it('should pass the correct friend profile picture', () => {
     const chat = { id: 1 };
     const friend = {
       profile: { username: 'friend1', picture: '/path/to/picture' },
     };
     render(<ChatLink chat={chat} friend={friend} />);
 
-    screen.debug();
     const image = screen.getByText('/path/to/picture');
     expect(image).toBeInTheDocument();
   });
 
-  // it('should render any children passed to it', () => {
-  //   const user = { profile: { picture: 'picture url', username: 'user1' } };
-  //   const active = true;
-  //   const scale = 1;
-  //   render(
-  //     <ChatLink user={user} active={active} scale={scale}>
-  //       <div>child 1</div>
-  //       <div>child 2</div>
-  //       <div>
-  //         <div>child 3</div>
-  //       </div>
-  //     </ChatLink>,
-  //   );
+  it('should render the friend username', () => {
+    const chat = { id: 1 };
+    const friend = {
+      profile: { username: 'friend1', picture: '/path/to/picture' },
+    };
+    render(<ChatLink chat={chat} friend={friend} />);
 
-  //   const child1 = screen.getByText('child 1');
-  //   expect(child1).toBeInTheDocument();
-  //   const child2 = screen.getByText('child 2');
-  //   expect(child2).toBeInTheDocument();
-  //   const child3 = screen.getByText('child 3');
-  //   expect(child3).toBeInTheDocument();
-  // });
+    const image = screen.getByText('friend1');
+    expect(image).toBeInTheDocument();
+  });
+
+  it('should render any children', () => {
+    const chat = { id: 1 };
+    const friend = {
+      profile: { username: 'friend1', picture: '/path/to/picture' },
+    };
+    render(
+      <ChatLink chat={chat} friend={friend}>
+        <div>children</div>
+      </ChatLink>,
+    );
+
+    const children = screen.getByText('children');
+    expect(children).toBeInTheDocument();
+  });
 });
