83de821cd2fc366a9ac7e1fe5c00230888f70730
Dylan Palmboom <nofxboy1234@gmail.com>
2025-04-15 16:45:17 +0200

    Experiment with eager loading queries in query_playground folder

---
+++ query_playground/chats_query_eager.rb
@@ -0,0 +1,10 @@
+chats = Chat.includes(:friendship).limit(10)
+
+chats.each do |chat|
+  puts chat.as_json(include: :friendship)
+  # puts chat.friendship.id
+end
+
+# s_chats = chats.as_json(include: :friendship)
+# # s_chats = chats.as_json
+# puts s_chats
