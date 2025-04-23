e844b5a190683932758e56e98feb378b96dc2a28
Dylan Palmboom <nofxboy1234@gmail.com>
2025-04-23 21:36:56 +0200

    Add e2e test for dismissing a confirm dialog

--- app/frontend/tests/e2e/app.spec.js
+++ app/frontend/tests/e2e/app.spec.js
@@ -87,7 +87,7 @@ test.describe('when navigating to the Home page', () => {
     await expect(page.getByRole('button', { name: 'Unfriend' })).toBeVisible();
   });
 
-  test('should show a popup message to confirm, and remove the friend and chat if OK is clicked', async ({
+  test('should show a popup message to confirm, and remove the friend and chat if accepted', async ({
     page,
   }) => {
     const chatIndex = page.getByTestId('chat-index');
@@ -127,6 +127,46 @@ test.describe('when navigating to the Home page', () => {
     await expect(
       friendIndex.getByTestId('user-link-/profiles/5'),
     ).not.toBeVisible();
+  });
+
+  test('should show a popup message to confirm, and do nothing if dismissed', async ({
+    page,
+  }) => {
+    const chatIndex = page.getByTestId('chat-index');
+    const friendIndex = page.getByTestId('friend-index');
+
+    await expect(page.getByText('CHATS-2')).toBeVisible();
+    await expect(chatIndex.getByRole('link', { name: 'user4' })).toBeVisible();
+    await expect(chatIndex.getByRole('link', { name: 'user5' })).toBeVisible();
+
+    await expect(page.getByText('ALL FRIENDS-2')).toBeVisible();
+    await expect(
+      friendIndex.getByTestId('user-link-/profiles/4'),
+    ).toBeVisible();
+    await expect(
+      friendIndex.getByTestId('user-link-/profiles/5'),
+    ).toBeVisible();
+
+    page.on('dialog', async (dialog) => {
+      expect(dialog.message()).toBe('Unfriend user5?');
+      await dialog.dismiss();
+    });
+
+    await friendIndex.getByTestId('user-link-/profiles/5').click();
+    const unfriend = page.getByRole('button', { name: 'Unfriend' });
+    await unfriend.click();
+
+    await expect(page.getByText('CHATS-2')).toBeVisible();
+    await expect(chatIndex.getByRole('link', { name: 'user4' })).toBeVisible();
+    await expect(chatIndex.getByRole('link', { name: 'user5' })).toBeVisible();
+
+    await expect(page.getByText('ALL FRIENDS-2')).toBeVisible();
+    await expect(
+      friendIndex.getByTestId('user-link-/profiles/4'),
+    ).toBeVisible();
+    await expect(
+      friendIndex.getByTestId('user-link-/profiles/5'),
+    ).toBeVisible();
   });
 
   test.describe('when clicking on the Log out button', () => {
