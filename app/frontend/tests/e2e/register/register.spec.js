ce6d619c1af9611e082788317c344f1242ee1643
Dylan Palmboom <nofxboy1234@gmail.com>
2025-05-01 21:00:21 +0200

    Update e2e tests for registering

--- app/frontend/tests/e2e/register/register.spec.js
+++ app/frontend/tests/e2e/register/register.spec.js
@@ -1,38 +1,47 @@
 import { test, expect } from '@playwright/test';
 import { execSync } from 'child_process';
+import path from 'path';
 
-const setup_test_data = async () => {
-  execSync('RAILS_ENV=test rails playwright:setup_test_data', {
-    stdio: 'inherit',
-  });
-};
-
-const cleanup_test_data = async () => {
-  execSync('RAILS_ENV=test rails playwright:cleanup_test_data', {
-    stdio: 'inherit',
+const setup_test_data_with_users = async () => {
+  return new Promise((resolve) => {
+    execSync('RAILS_ENV=test rails playwright:setup_test_data', {
+      stdio: 'inherit',
+    });
+    resolve();
   });
 };
 
 test.beforeEach(async ({ page }) => {
-  await setup_test_data();
+  await setup_test_data_with_users();
+  await page.goto('/users/sign_in');
+  await page.waitForURL('/users/sign_in');
+});
 
-  await page.goto('/');
+test.afterEach(async ({ page }) => {
+  await setup_test_data_with_users();
+
+  console.log('*** setup authenticated state ***');
+  // Perform authentication steps. Replace these actions with your own.
+  await page.goto('/users/sign_in');
   await page.getByLabel('Email:').fill('user1@example.com');
   await page.getByLabel('Password:').fill('123456');
   await page.getByRole('button', { name: 'Log in' }).click();
+  // Wait until the page receives the cookies.
+  //
+  // Sometimes login flow sets cookies in the process of several redirects.
+  // Wait for the final URL to ensure that the cookies are actually set.
+  // await page.waitForURL('/');
+  // Alternatively, you can wait until the page reaches a state where all cookies are set.
+  await expect(page.getByText('Profile (user1)')).toBeVisible();
+  // End of authentication steps.
+  const authFile = path.join(
+    import.meta.dirname,
+    '../playwright/.auth/user.json',
+  );
+  await page.context().storageState({ path: authFile });
 });
 
-test.afterEach(async () => {
-  await cleanup_test_data();
-});
-
-test.describe('when navigating to any page while signed out', () => {
-  test.beforeEach(async ({ page }) => {
-    await setup_test_data();
-
-    await page.goto('/chats/3');
-  });
-
+test.describe('when on the sign up page', () => {
   test('should show the sign up screen when clicking "Sign up"', async ({
     page,
   }) => {
@@ -125,5 +134,34 @@ test.describe('when navigating to any page while signed out', () => {
       page.getByText('Welcome! You have signed up successfully.'),
     ).toBeVisible();
     await expect(page.getByText('Profile (user99)')).toBeVisible();
+  });
+
+  test('should show "has already been taken" message when signing up with an email that is already registered', async ({
+    page,
+  }) => {
+    await page.getByRole('button', { name: 'Sign up' }).click();
+
+    const registrationsNew = page.getByTestId('registrations-new');
+
+    await registrationsNew.getByLabel('Email:').fill('user99@example.com');
+    await registrationsNew.getByLabel('Password:').fill('123456');
+    await registrationsNew.getByLabel('Password confirmation:').fill('123456');
+    await registrationsNew.getByRole('button', { name: 'Sign up' }).click();
+
+    await expect(
+      page.getByText('Welcome! You have signed up successfully.'),
+    ).toBeVisible();
+    await expect(page.getByText('Profile (user99)')).toBeVisible();
+
+    await page.getByRole('button', { name: 'Log out' }).click();
+
+    await page.getByRole('button', { name: 'Sign up' }).click();
+
+    await registrationsNew.getByLabel('Email:').fill('user99@example.com');
+    await registrationsNew.getByLabel('Password:').fill('123456');
+    await registrationsNew.getByLabel('Password confirmation:').fill('123456');
+    await registrationsNew.getByRole('button', { name: 'Sign up' }).click();
+
+    await expect(page.getByText('has already been taken')).toBeVisible();
   });
 });
