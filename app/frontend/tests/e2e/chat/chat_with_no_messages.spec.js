384679e66fb3e193896f29fced3b5373ce6291fd
Dylan Palmboom <nofxboy1234@gmail.com>
2025-05-01 11:03:16 +0200

    Add chat e2e test for no messages

---
+++ app/frontend/tests/e2e/chat/chat_with_no_messages.spec.js
@@ -0,0 +1,34 @@
+import { expect, test } from '@playwright/test';
+import { execSync } from 'child_process';
+
+const setup_test_data_except_users = async () => {
+  execSync('RAILS_ENV=test rails playwright:setup_test_data_except_users', {
+    stdio: 'inherit',
+  });
+};
+
+// const cleanup_test_data_except_users = async () => {
+//   execSync('RAILS_ENV=test rails playwright:cleanup_test_data_except_users', {
+//     stdio: 'inherit',
+//   });
+// };
+
+// test.beforeEach(async ({ page }) => {
+//   await setup_test_data_except_users();
+//   await page.goto('/chats/2');
+//   await page.waitForURL('/chats/2');
+// });
+
+// test.afterEach(async () => {
+//   await cleanup_test_data_except_users();
+// });
+
+test.describe('when there are no messages', () => {
+  test('should show an empty chat', async ({ page }) => {
+    await setup_test_data_except_users();
+    await page.goto('/chats/2');
+    await page.waitForURL('/chats/2');
+
+    await expect(page.getByTestId('message')).toHaveCount(0);
+  });
+});
