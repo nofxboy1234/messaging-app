95917b8229fdce89545a7c0309c6605c8a8731db
Dylan Palmboom <nofxboy1234@gmail.com>
2025-05-01 22:48:08 +0200

    Update sign in e2e tests

--- app/frontend/tests/e2e/signIn/signIn.spec.js
+++ app/frontend/tests/e2e/signIn/signIn.spec.js
@@ -1,48 +1,53 @@
 import { test, expect } from '@playwright/test';
 import { execSync } from 'child_process';
+import path from 'path';
 
-const setup_test_data = async () => {
-  execSync('RAILS_ENV=test rails playwright:setup_test_data', {
-    stdio: 'inherit',
-  });
-};
-
-const cleanup_test_data = async () => {
-  execSync('RAILS_ENV=test rails playwright:cleanup_test_data', {
-    stdio: 'inherit',
+const setup_test_data_with_users = async () => {
+  return new Promise((resolve) => {
+    execSync('RAILS_ENV=test rails playwright:setup_test_data', {
+      stdio: 'inherit',
+    });
+    resolve();
   });
 };
 
 test.beforeEach(async ({ page }) => {
-  await setup_test_data();
+  await setup_test_data_with_users();
+  await page.goto('/users/sign_in');
+  // await page.waitForURL('/users/sign_in');
+  await page.waitForLoadState();
+});
 
-  await page.goto('/');
+test.afterEach(async ({ page }) => {
+  await setup_test_data_with_users();
+
+  console.log('### setup authenticated state ###');
+  // Perform authentication steps. Replace these actions with your own.
+  await page.goto('/users/sign_in');
   await page.getByLabel('Email:').fill('user1@example.com');
   await page.getByLabel('Password:').fill('123456');
   await page.getByRole('button', { name: 'Log in' }).click();
+  // Wait until the page receives the cookies.
+  //
+  // Sometimes login flow sets cookies in the process of several redirects.
+  // Wait for the final URL to ensure that the cookies are actually set.
+  // await page.waitForURL('/');
+  // Alternatively, you can wait until the page reaches a state where all cookies are set.
+  await expect(page.getByText('Profile (user1)')).toBeVisible();
+  // End of authentication steps.
+  const authFile = path.join(
+    import.meta.dirname,
+    '../../../playwright/.auth/user.json',
+  );
+  await page.context().storageState({ path: authFile });
 });
 
-test.afterEach(async () => {
-  await cleanup_test_data();
-});
-
-test.describe('when navigating to any page while signed out', () => {
-  test.beforeEach(async ({ page }) => {
-    await setup_test_data();
-
-    await page.goto('/chats/3');
-  });
-
-  test('should show the login screen with a flash message', async ({
-    page,
-  }) => {
+test.describe('when on the sign in page', () => {
+  test('should show the login screen inputs and buttons', async ({ page }) => {
     await expect(page.getByLabel('Email:')).toBeVisible();
     await expect(page.getByLabel('Password:')).toBeVisible();
     await expect(page.getByRole('button', { name: 'Log in' })).toBeVisible();
     await expect(page.getByRole('button', { name: 'Sign up' })).toBeVisible();
-    await expect(
-      page.getByText('You need to sign in or sign up before continuing.'),
-    ).toBeVisible();
   });
 
   test('should show "Invalid Email or password" when entering a valid email then clicking "Log in"', async ({
