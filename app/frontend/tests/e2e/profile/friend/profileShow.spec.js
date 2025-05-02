3c999b3923727569dec9f58a4781e2ed06f5ffd7
Dylan Palmboom <nofxboy1234@gmail.com>
2025-05-02 20:36:52 +0200

    Refactor friend profile show e2e tests

--- app/frontend/tests/e2e/profile/friend/profileShow.spec.js
+++ app/frontend/tests/e2e/profile/friend/profileShow.spec.js
@@ -1,7 +1,17 @@
-import { expect } from '@playwright/test';
-import setupTest from '../../setupTest';
+import { expect, test } from '@playwright/test';
+import { execSync } from 'child_process';
 
-const test = setupTest(async ({ page }) => {
+const setup_test_data_except_users = async () => {
+  return new Promise((resolve) => {
+    execSync('RAILS_ENV=test rails playwright:setup_test_data_except_users', {
+      stdio: 'inherit',
+    });
+    resolve();
+  });
+};
+
+test.beforeEach(async ({ page }) => {
+  await setup_test_data_except_users();
   await page.goto('/profiles/4');
   await page.waitForURL('/profiles/4');
 });
