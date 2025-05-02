798a071aed93e9a3d566ef8b8c25c9ac72f327f5
Dylan Palmboom <nofxboy1234@gmail.com>
2025-05-02 10:50:07 +0200

    Refactor friend requests e2e tests

--- app/frontend/tests/e2e/friendRequests/friendRequests.spec.js
+++ app/frontend/tests/e2e/friendRequests/friendRequests.spec.js
@@ -1,7 +1,17 @@
-import { expect } from '@playwright/test';
-import test from '../setupTest';
+import { expect, test } from '@playwright/test';
+import { execSync } from 'child_process';
+
+const setup_test_data_except_users = async () => {
+  return new Promise((resolve) => {
+    execSync('RAILS_ENV=test rails playwright:setup_test_data_except_users', {
+      stdio: 'inherit',
+    });
+    resolve();
+  });
+};
 
 test.beforeEach(async ({ page }) => {
+  await setup_test_data_except_users();
   await page.goto('/pending_friends');
   await page.waitForURL('/pending_friends');
 });
