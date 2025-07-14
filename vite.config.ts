8ac315ca8ff4c28eaa439f5d3d5673868184d122
Dylan Palmboom <nofxboy1234@gmail.com>
2025-07-14 18:28:58 +0200

    Revert layout, application.html.erb and vite config in prep for NPM_CONFIG_INCLUDE env var set on render.com

--- vite.config.ts
+++ vite.config.ts
@@ -21,19 +21,4 @@ export default defineConfig({
     testTimeout: 0,
     fileParallelism: false,
   },
-  build: {
-    rollupOptions: {
-      input: {
-        // Keep your JS entrypoints separate
-        application: './app/frontend/entrypoints/application.js',
-        inertia: './app/frontend/entrypoints/inertia.js',
-        // Make index.css its own, distinct entrypoint
-        // Use a unique name like 'global_styles_css' to avoid any naming conflicts
-        global_styles_css: './app/frontend/index.css',
-      },
-    },
-    // Ensure CSS is extracted and not inlined
-    cssCodeSplit: true, // This is default, but good to be explicit
-    assetsInlineLimit: 0, // Ensure even small CSS files are NOT inlined into JS
-  },
 });
